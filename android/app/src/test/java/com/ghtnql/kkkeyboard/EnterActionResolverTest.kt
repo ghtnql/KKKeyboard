97426ae7629d819f3a1e01daf7faa00c190d5f1f
ghtnql <52940761+ghtnql@users.noreply.github.com>
2026-09-13 04:32:17 +0900

    Add editor action regression tests

---
+++ android/app/src/test/java/com/ghtnql/kkkeyboard/EnterActionResolverTest.kt
@@ -0,0 +1,34 @@
+package com.ghtnql.kkkeyboard
+
+import android.view.inputmethod.EditorInfo
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class EnterActionResolverTest {
+    @Test
+    fun resolvesCommonEditorActions() {
+        listOf(
+            EditorInfo.IME_ACTION_GO,
+            EditorInfo.IME_ACTION_SEARCH,
+            EditorInfo.IME_ACTION_SEND,
+            EditorInfo.IME_ACTION_NEXT,
+            EditorInfo.IME_ACTION_DONE,
+            EditorInfo.IME_ACTION_PREVIOUS,
+        ).forEach { action ->
+            assertEquals(action, EnterActionResolver.actionId(action))
+        }
+    }
+
+    @Test
+    fun returnsNullForUnspecifiedOrNone() {
+        assertNull(EnterActionResolver.actionId(EditorInfo.IME_ACTION_UNSPECIFIED))
+        assertNull(EnterActionResolver.actionId(EditorInfo.IME_ACTION_NONE))
+    }
+
+    @Test
+    fun noEnterActionFlagForcesPlainEnter() {
+        val options = EditorInfo.IME_ACTION_SEND or EditorInfo.IME_FLAG_NO_ENTER_ACTION
+        assertNull(EnterActionResolver.actionId(options))
+    }
+}
