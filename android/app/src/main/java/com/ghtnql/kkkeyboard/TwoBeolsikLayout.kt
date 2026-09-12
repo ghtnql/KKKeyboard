c0f3e192393a50804cc05881791f6b30761fae29
ghtnql <52940761+ghtnql@users.noreply.github.com>
2026-09-13 03:36:37 +0900

    Extract two-beolsik layout mapping for testable shift behavior

---
+++ android/app/src/main/java/com/ghtnql/kkkeyboard/TwoBeolsikLayout.kt
@@ -0,0 +1,25 @@
+package com.ghtnql.kkkeyboard
+
+object TwoBeolsikLayout {
+    val characterRows: List<List<String>> = listOf(
+        listOf("ㅂ", "ㅈ", "ㄷ", "ㄱ", "ㅅ", "ㅛ", "ㅕ", "ㅑ", "ㅐ", "ㅔ"),
+        listOf("ㅁ", "ㄴ", "ㅇ", "ㄹ", "ㅎ", "ㅗ", "ㅓ", "ㅏ", "ㅣ"),
+    )
+
+    val bottomRow: List<String> = listOf("ㅋ", "ㅌ", "ㅊ", "ㅍ", "ㅠ", "ㅜ", "ㅡ")
+
+    private val shiftedKeys = mapOf(
+        "ㅂ" to "ㅃ",
+        "ㅈ" to "ㅉ",
+        "ㄷ" to "ㄸ",
+        "ㄱ" to "ㄲ",
+        "ㅅ" to "ㅆ",
+        "ㅐ" to "ㅒ",
+        "ㅔ" to "ㅖ",
+    )
+
+    fun labelFor(baseLabel: String, shifted: Boolean): String =
+        if (shifted) shiftedKeys[baseLabel] ?: baseLabel else baseLabel
+
+    fun hasShiftVariant(baseLabel: String): Boolean = shiftedKeys.containsKey(baseLabel)
+}
