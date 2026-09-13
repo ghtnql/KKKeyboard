4bef906d4103907e58f2cf56340b75efc0e7e8ae
ghtnql <52940761+ghtnql@users.noreply.github.com>
2026-09-13 12:32:49 +0900

    Add iOS Hangul composer unit tests

---
+++ ios/Tests/HangulComposerTests.swift
@@ -0,0 +1,51 @@
+import XCTest
+@testable import KKKeyboardExtension
+
+final class HangulComposerTests: XCTestCase {
+    func testComposesSimpleSyllable() {
+        let composer = HangulComposer()
+        XCTAssertEqual(composer.input("ㄱ").composing, "ㄱ")
+        XCTAssertEqual(composer.input("ㅏ").composing, "가")
+    }
+
+    func testMovesFinalConsonantToNextSyllable() {
+        let composer = HangulComposer()
+        _ = composer.input("ㄱ")
+        _ = composer.input("ㅏ")
+        XCTAssertEqual(composer.input("ㄴ").composing, "간")
+
+        let edit = composer.input("ㅏ")
+        XCTAssertEqual(edit.commit, "가")
+        XCTAssertEqual(edit.composing, "나")
+    }
+
+    func testCompoundVowelAndBackspace() {
+        let composer = HangulComposer()
+        _ = composer.input("ㅇ")
+        _ = composer.input("ㅗ")
+        XCTAssertEqual(composer.input("ㅏ").composing, "와")
+        XCTAssertEqual(composer.backspace().composing, "오")
+    }
+
+    func testCompoundFinalSplitsBeforeVowel() {
+        let composer = HangulComposer()
+        _ = composer.input("ㄷ")
+        _ = composer.input("ㅏ")
+        _ = composer.input("ㄹ")
+        XCTAssertEqual(composer.input("ㄱ").composing, "닭")
+
+        let edit = composer.input("ㅏ")
+        XCTAssertEqual(edit.commit, "달")
+        XCTAssertEqual(edit.composing, "가")
+    }
+
+    func testFlushClearsState() {
+        let composer = HangulComposer()
+        _ = composer.input("ㅎ")
+        _ = composer.input("ㅏ")
+        _ = composer.input("ㄴ")
+        XCTAssertEqual(composer.flush(), "한")
+        XCTAssertEqual(composer.currentText(), "")
+        XCTAssertFalse(composer.backspace().consumed)
+    }
+}
